07c6cd3a1657638f27835e1376bd5d59d079efaf
Levi Barreto Araújo <levibaraujo80@gmail.com>
2023-05-10 09:36:32 -0300

    feat: email verify if exists middleware

---
+++ src/middlewares/validateEmail.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../error";
+import { Repository } from "typeorm";
+import User from "../entities/users";
+import { AppDataSource } from "../data-source";
+
+const validateEmail = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.body.email) {
+    throw new AppError("Email is missing", 400);
+  }
+
+  const userRepository: Repository<User> = AppDataSource.getRepository(User);
+
+  const user: User | null = await userRepository.findOne({
+    where: {
+      email: req.body.email,
+    },
+  });
+
+  if (user !== null) {
+    throw new AppError("User already registered", 409);
+  }
+
+  return next();
+};
+
+export default validateEmail;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createUser } from "../controllers/users";
+import validateEmail from "../middlewares/validateEmail";
 
 const userRoutes: Router = Router();
 
-userRoutes.post("", createUser);
+userRoutes.post("", validateEmail, createUser);
 
 export default userRoutes;
